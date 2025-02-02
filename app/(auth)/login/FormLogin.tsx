905f712626888294206d22a13c165730220b5671
daviirb <davi.rb@hotmail.com>
2025-02-02 17:36:39 -0300

    feat: implements login

---
+++ app/(auth)/login/FormLogin.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { Button } from "@/components/ui/Button";
+import { Checkbox } from "@/components/ui/Checkbox";
+import { Input } from "@/components/ui/Input";
+import { Lock, MailIcon } from "lucide-react";
+import Form from "next/form";
+import Link from "next/link";
+import { useActionState } from "react";
+import loginAction from "./loginAction";
+
+export function FormLogin() {
+  const [state, formAction, isPending] = useActionState(loginAction, null);
+
+  return (
+    <>
+      {state?.success === false && (
+        <div>
+          <strong>Erro!</strong>
+          <span>{state.message}</span>
+        </div>
+      )}
+      <Form className="space-y-6" action={formAction}>
+        <div className="relative">
+          <Input
+            name="email"
+            type="email"
+            placeholder="seu.email@exemplo.com"
+          />
+          <MailIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-colorGray h-5 w-5" />
+        </div>
+        <div className="relative">
+          <Input name="password" type="password" placeholder="senha" />
+          <Lock className="absolute right-3 top-1/2 -translate-y-1/2 text-colorGray h-5 w-5" />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2">
+            <Checkbox id="remember" />
+            <label
+              htmlFor="remember"
+              className="text-sm text-gray-500 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Lembrar de mim
+            </label>
+          </div>
+          <Button variant="link">Recuperar senha</Button>
+        </div>
+        <div>
+          <Button type="submit" className="w-full">
+            Entrar
+          </Button>
+        </div>
+        <div>
+          <Link href="/cadastro">
+            <Button className="w-full" variant="outline">
+              Criar Conta
+            </Button>
+          </Link>
+        </div>
+      </Form>
+    </>
+  );
+}
---
+++ app/(auth)/login/loginAction.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export default async function loginAction(_prevState: any, formData: FormData) {
+  try {
+    await signIn("credentials", {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+      redirect: false,
+    });
+    return {
+      success: true,
+    };
+  } catch (error: unknown) {
+    if (error instanceof AuthError && error.type === "CredentialsSignin") {
+      return {
+        success: false,
+        message: "Dados de login incorretos.",
+      };
+    }
+    return {
+      success: false,
+      message: "Ops, algum erro aconteceu!",
+    };
+  }
+}
