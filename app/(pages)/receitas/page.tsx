5bd089c49f55648001e6b6f79a71340490537f04
daviirb <davi.rb@hotmail.com>
2025-02-16 22:57:54 -0300

    chore: remove access to the recipe page for unsubscribed users

--- app/(pages)/receitas/page.tsx
+++ app/(pages)/receitas/page.tsx
@@ -1,32 +1,53 @@
 import Link from 'next/link';
 
+import { auth } from '@/auth';
+import BannerWarning from '@/components/BannerWarning';
+import PricingCard from '@/components/PricingCard';
 import { Button } from '@/components/ui/Button';
+import { fetchSubscriptionByEmail } from '@/lib/stripe';
 
 import { RecipeCard } from './_components/RecipeCard';
 
-export default function RecipePage() {
+export default async function RecipePage() {
+  const session = await auth();
+  let subscription = null;
+
+  if (session) {
+    const email = session?.user?.email;
+    subscription = await fetchSubscriptionByEmail(email ?? '');
+  }
   return (
     <main className="flex w-full flex-col p-2">
       <header className="mb-4">
         <h1 className="text-2xl font-bold">Receitas Fit</h1>
       </header>
-      <section>
-        <div className="grid grid-cols-2 place-items-center gap-y-4 md:gap-x-8 lg:grid-cols-3">
-          <RecipeCard
-            recipe={panquecafit}
-            name="Panqueca Fit"
-            src="/panquecafit.webp"
-            alt="Image da Panqueca"
-            time={8}
-            calories={225}
-          />
-        </div>
-      </section>
-      <Button variant="link" className="mt-16">
-        <Link href={'https://pay.kiwify.com.br/SPf7mbY'} target="_blank">
-          Descubra Mais Receitas Receitas Poderosas
-        </Link>
-      </Button>
+      {subscription && (
+        <>
+          <section>
+            <div className="grid grid-cols-2 place-items-center gap-y-4 md:gap-x-8 lg:grid-cols-3">
+              <RecipeCard
+                recipe={panquecafit}
+                name="Panqueca Fit"
+                src="/panquecafit.webp"
+                alt="Image da Panqueca"
+                time={8}
+                calories={225}
+              />
+            </div>
+          </section>
+          <Button variant="link" className="mt-16">
+            <Link href={'https://pay.kiwify.com.br/SPf7mbY'} target="_blank">
+              Descubra Mais Receitas Receitas Poderosas
+            </Link>
+          </Button>
+        </>
+      )}
+      {!subscription && (
+        <>
+          <BannerWarning text="Você não possui nenhuma assinatura ativa. Que tal assinar agora? " />
+          <PricingCard />
+        </>
+      )}
     </main>
   );
 }
