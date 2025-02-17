b3058063305e1996b207c1d9e46ce464a81bf3a9
daviirb <davi.rb@hotmail.com>
2025-02-16 22:24:24 -0300

    feat: adds facebook pixel event tracker

--- app/(checkout)/payment-confirmation/layout.tsx
+++ app/(checkout)/payment-confirmation/layout.tsx
@@ -1,6 +1,16 @@
-import { ReactNode } from 'react';
+'use client';
+import { ReactNode, useEffect } from 'react';
+
+import { event } from '@/lib/fpixel';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    event('Subscribe', {
+      value: 19.9,
+      currency: 'BRL',
+      predicted_ltv: 19.9,
+    });
+  });
   return (
     <section className="flex flex-col items-center justify-center py-40">
       {children}
